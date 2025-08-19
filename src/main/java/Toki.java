f21a2e193e2b0d8eff36886451edf764ac837154
NownuJ <junwon2178@gmail.com>
2025-08-19 18:42:43 +0800

    Level 1. Echo

--- src/main/java/Toki.java
+++ src/main/java/Toki.java
@@ -1,15 +1,33 @@
+import java.util.Scanner;
+
 public class Toki {
+    private static final String LINE =
+            "   ____________________________________________________________";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println(LINE);
+        System.out.println("    Hello! I'm Toki");
+        System.out.println("    What can I do for you?");
+        System.out.println(LINE);
 
-        System.out.println(
-                " Hello! I'm, Toki\n" +
-                " What can I do for you?\n\n" +
-                " Bye. Hope to see you again soon!");
+        try (Scanner sc = new Scanner(System.in)) {
+            while (true) {
 
+                String input  = sc.nextLine();
+
+                if (input.equals("bye")) {
+                    System.out.println(LINE);
+                    System.out.println("    Bye. Hope to see you again soon!");
+                    System.out.println(LINE);
+                    break;
+                }
+
+                System.out.println(LINE);
+                System.out.println("    " + input);
+                System.out.println(LINE);
+            }
+        }
     }
+
+
 }
